18c389a99f62fef97140d8327d163d72b754e6c7
Cannon, Robert <Robert.Cannon@turner.com>
2018-06-11 10:21:01 -0400

    Code to remove unwanted store apps

--- README.md
+++ README.md
@@ -4,19 +4,17 @@ This is my personal boxstarter script
 
 This is based on these projects
 
-* Boxstarter [boxstarter.org](http://boxstarter.org)
-* Chocolatey [chocolatey.org](http://chocolatey.org)
-* Windows Dev Box setup (https://github.com/Microsoft/windows-dev-box-setup-scripts)
+- Boxstarter [boxstarter.org](http://boxstarter.org)
+- Chocolatey [chocolatey.org](http://chocolatey.org)
+- Windows Dev Box setup (https://github.com/Microsoft/windows-dev-box-setup-scripts)
 
 ## How to run the scripts
 
 To run a setup script, click a link in the table below from your target machine. This will download Boxstarter, and prompt you for Boxstarter to run with Administrator privileges (which it needs to do its job). Clicking yes in this dialog will cause the script to begin. You can then leave the job unattended and come back when it's finished.
 
-| Click link to run                                                                                                                                                                           | Description   |
-| ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------- |
-| <a href='http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/RobCannon/boxstarter/master/dev_box.ps1?token=AK0OHkKpCgt-8lIcqBiQ1amsoPRrsDB7ks5a-J-0wA%3D%3D'>dev_box</a> | My Boxstarter |
-
-
+| Click link to run                                                                                                                    | Description   |
+| ------------------------------------------------------------------------------------------------------------------------------------ | ------------- |
+| <a href='http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/RobCannon/boxstarter/master/dev_box.ps1'>dev_box</a> | My Boxstarter |
 
 #### Setup up WSL via curl
 
--- dev_box.ps1
+++ dev_box.ps1
@@ -108,6 +108,36 @@ Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\
 # Enable PIN and Windows Hello
 Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\System -name AllowDomainPINLogon -value 1
 
+# Remove Windows Store Apps
+Get-AppxPackage Microsoft.3DBuilder | Remove-AppxPackage
+Get-AppxPackage *Autodesk* | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingFinance | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingSports | Remove-AppxPackage
+Get-AppxPackage *BubbleWitch* | Remove-AppxPackage
+Get-AppxPackage king.com.CandyCrush* | Remove-AppxPackage
+Get-AppxPackage Microsoft.CommsPhone | Remove-AppxPackage
+Get-AppxPackage *Dropbox* | Remove-AppxPackage
+Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage
+Get-AppxPackage *Keeper* | Remove-AppxPackage
+Get-AppxPackage *MarchofEmpires* | Remove-AppxPackage
+Get-AppxPackage Microsoft.Messaging | Remove-AppxPackage
+Get-AppxPackage *Minecraft* | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+Get-AppxPackage Microsoft.OneConnect | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsPhone | Remove-AppxPackage
+Get-AppxPackage *Plex* | Remove-AppxPackage
+Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsSoundRecorder | Remove-AppxPackage
+Get-AppxPackage *Solitaire* | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
+Get-AppxPackage Microsoft.Office.Sway | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage
+Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
+Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage
+
+
 #--- Tools ---
 choco install -y vscode
 Get-ChildItem "$([Environment]::GetFolderPath('CommonDesktopDirectory'))" | ? { $_.Name -eq 'Visual Studio Code.lnk' } | Remove-Item
